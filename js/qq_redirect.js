3fd1c84ce708af35cca68836464ca4e99a90b83c
revice <59417283+Revicess@users.noreply.github.com>
2024-01-17 03:39:14 +0800

    更新 qq_redirect.js

--- js/qq_redirect.js
+++ js/qq_redirect.js
@@ -1,5 +1,16 @@
+//匹配链接来源：https://gitlab.com/lodepuly/vpn_tool/-/raw/master/Tool/Loon/Plugin/QQ_Redirect.plugin
+/*
+^https:\/\/c\.pc\.qq\.com\/(ios|index|middlem)\.html\?(pf)?url= url script-response-header https://raw.githubusercontent.com/Revicess/script/master/js/qq_redirect.js
+
+^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/(ios|middlem)\.html\?(pf)?url%3d url script-response-header https://raw.githubusercontent.com/Revicess/script/master/js/qq_redirect.js
+
+^https:\/\/cgi\.connect\.qq\.com\/qqconnectopen\/get_urlinfoForQQV2\?url= url script-response-header https://raw.githubusercontent.com/Revicess/script/master/js/qq_redirect.js
+
+hostname = pingtas.qq.com, c.pc.qq.com, cgi.connect.qq.com
+*/
+
 let url = $request.url;
 //判断url是否为二次encode
 if(url.includes("%25"))url = decodeURIComponent(url);
-let redirect = decodeURIComponent(url.match(/\?(?:pf|)url=([^&]+)/)[1]);
+let redirect = decodeURIComponent(url.match(/\?(?:pf)?url=([^&]+)/)[1]);
 $done({ status: "HTTP/1.1 302 Moved Temporarily", headers: { Location: redirect}});
