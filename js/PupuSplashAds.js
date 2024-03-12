aa68e4eafcdb30745b7db6b274bfd2a10d15109d
revice <59417283+Revicess@users.noreply.github.com>
2024-03-12 20:14:38 +0800

    Update PupuSplashAds.js

--- js/PupuSplashAds.js
+++ js/PupuSplashAds.js
@@ -5,13 +5,13 @@
 http-response ^https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2[%A-Z0-9]+&store_id script-path=https://raw.githubusercontent.com/Revicess/script/master/js/PupuSplashAds.js, requires-body=true, tag=朴朴超市开屏广告
 
 [MITM]
-hostname=j1.pupuapi.com,140.179.224.63
+hostname=j1.pupuapi.com
 
 //圈x远程引用
-//^http:\/\/(54\.222\.159\.138:8053|161\.189\.16\.5:8053|dnsazure\.pupuapi\.com)\/httpdns\/resolve\?domain=j1\.pupuapi\.com$ url reject-dict
-//^https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2[%A-Z0-9]+&store_id url script-response-body https://raw.githubusercontent.com/Revicess/script/master/js/PupuSplashAds.js
-//httpdns ip 140.179.224.63
-hostname=j1.pupuapi.com,140.179.224.63
+^http:\/\/(54\.222\.159\.138:8053|161\.189\.16\.5:8053|dnsazure\.pupuapi\.com)\/httpdns\/resolve\?domain=j1\.pupuapi\.com$ url reject-dict
+^https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2[%A-Z0-9]+&store_id url script-response-body https://raw.githubusercontent.com/Revicess/script/master/js/PupuSplashAds.js
+
+hostname=j1.pupuapi.com
 */
 
 let obj=JSON.parse($response.body);
